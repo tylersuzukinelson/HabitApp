78207e1d2512f68ea59125b651830a6371577395
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-23 17:40:28 -0800

    Added function to create habits, and added restrictions to habit model

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -4,4 +4,24 @@ class HabitsController < ApplicationController
   def new
   end
 
+  def create
+    @habit = Habit.new permitted_params
+    if @habit.save
+      redirect_to habits_path
+    else
+      flash[:notice] = error_messages
+      render "new"
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:habit).permit(:title, :description, :interval, :forgiveness)
+  end
+
+  def error_messages
+    @habit.errors.full_messages.join('; ')
+  end
+
 end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,16 @@
 class Habit < ActiveRecord::Base
   belongs_to :user
   has_many :logs, dependent: :destroy
+  validates :title, presence: true
+  validates :interval, numericality: {greater_than: 0, only_integer: true}
+  validates :forgiveness, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validate :set_defaults
+
+  private
+
+  def set_defaults
+    self.longest_streak = 0
+    self.current_streak = 0
+    self.public = false
+  end
 end
